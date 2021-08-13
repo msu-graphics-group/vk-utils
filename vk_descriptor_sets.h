34b43f18e7af20948189958d45b5b8c982f9b3b9
vs <vadim.sanzharov@graphics.cs.msu.ru>
2021-08-13 13:44:41 +0300

    descriptor helper descriptor hashing

--- vk_descriptor_sets.h
+++ vk_descriptor_sets.h
@@ -4,6 +4,8 @@
 #define USE_VOLK
 #include "vk_include.h"
 
+#include "vk_descriptor_helpers.h"
+
 #include <string>
 #include <vector>
 #include <cassert>
@@ -12,65 +14,48 @@
 namespace vk_utils
 {
 
-  VkDescriptorSetLayout createDescriptorSetLayout(VkDevice a_device, const std::vector<std::pair<VkDescriptorType, uint32_t>> &a_descrTypes,
-                                                  VkShaderStageFlags a_stage = VK_SHADER_STAGE_COMPUTE_BIT);
-  VkDescriptorPool createDescriptorPool(VkDevice a_device, const std::vector<std::pair<VkDescriptorType, uint32_t>> &a_descrTypes,
-                                        unsigned a_maxSets);
-  VkDescriptorSet createDescriptorSet(VkDevice a_device, VkDescriptorSetLayout a_pDSLayout, VkDescriptorPool a_pDSPool,
-                                      const std::vector<VkDescriptorBufferInfo> &bufInfos);
+  VkDescriptorSetLayout createDescriptorSetLayout(VkDevice a_device, const DescriptorTypesVec &a_descrTypes, VkShaderStageFlags a_stage = VK_SHADER_STAGE_COMPUTE_BIT);
+  VkDescriptorPool createDescriptorPool(VkDevice a_device, const DescriptorTypesVec &a_descrTypes, unsigned a_maxSets);
+  VkDescriptorSet createDescriptorSet(VkDevice a_device, VkDescriptorSetLayout a_pDSLayout, VkDescriptorPool a_pDSPool, const std::vector<VkDescriptorBufferInfo> &bufInfos);
 
   class DescriptorMaker
   {
   public:
-    DescriptorMaker(VkDevice a_device, const std::vector<std::pair<VkDescriptorType, uint32_t>> &a_maxDescrTypes, uint32_t a_maxSets);
+    DescriptorMaker(VkDevice a_device, const DescriptorTypesVec &a_maxDescrTypes, uint32_t a_maxSets);
     ~DescriptorMaker();
 
-    void BindBegin (VkShaderStageFlags a_shaderStage);
+    void BindBegin(VkShaderStageFlags a_shaderStage);
     void BindBuffer(uint32_t a_loc, VkBuffer a_buffer, VkBufferView a_buffView = VK_NULL_HANDLE, VkDescriptorType a_bindType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER);
     void BindAccelStruct(uint32_t a_loc, VkAccelerationStructureKHR a_accStruct, VkDescriptorType a_bindType = VK_DESCRIPTOR_TYPE_ACCELERATION_STRUCTURE_KHR);
-    void BindImage (uint32_t a_loc, VkImageView  a_imageView, VkSampler a_sampler = VK_NULL_HANDLE, VkDescriptorType a_bindType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE,
-                            VkImageLayout a_imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-    void BindImageArray (uint32_t a_loc, const std::vector<VkImageView> &a_imageView, const std::vector<VkSampler> &a_sampler,
-                                VkDescriptorType a_bindType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-                                VkImageLayout a_imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
-    void BindEnd (VkDescriptorSet* a_pSet = nullptr, VkDescriptorSetLayout* a_pLayout = nullptr);
+    void BindImage(uint32_t a_loc, VkImageView a_imageView, VkSampler a_sampler = VK_NULL_HANDLE, VkDescriptorType a_bindType = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, VkImageLayout a_imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+    void BindImageArray(uint32_t a_loc, const std::vector<VkImageView> &a_imageView, const std::vector<VkSampler> &a_sampler, VkDescriptorType a_bindType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VkImageLayout a_imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
+    void BindEnd(VkDescriptorSet *a_pSet = nullptr, VkDescriptorSetLayout *a_pLayout = nullptr);
 
-//    VkDescriptorSetLayout GetLayout(uint32_t a_setId) const {}
-//    VkDescriptorSet       GetSet(uint32_t a_setId)    const {}
+    //    VkDescriptorSetLayout GetLayout(uint32_t a_setId) const {}
+    //    VkDescriptorSet       GetSet(uint32_t a_setId)    const {}
 
-    VkDescriptorPool      GetPool() const { return m_pool; }
+    VkDescriptorPool GetPool() const { return m_pool; }
 
   private:
     VkDevice m_device = VK_NULL_HANDLE;
     VkDescriptorPool m_pool = VK_NULL_HANDLE;
 
-    struct Handles
-    {
-      VkBufferView buffView     = VK_NULL_HANDLE;
-      VkBuffer     buffer       = VK_NULL_HANDLE;
-      std::vector<VkImageView>  imageView;
-      std::vector<VkSampler>    imageSampler;
-      VkAccelerationStructureKHR accelStruct = VK_NULL_HANDLE;
-
-      VkDescriptorType type = VK_DESCRIPTOR_TYPE_MAX_ENUM;
-      VkImageLayout imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-    };
-
-    std::unordered_map<uint32_t, Handles> m_bindings; // shader location to vk handle(s) array
-
     VkShaderStageFlags m_currentStageFlags = 0u;
+    std::unordered_map<uint32_t, DescriptorHandles> m_bindings;// shader location to vk handle(s)
 
-//    struct DSData
-//    {
-//      DSData() { sets.reserve(4); }
-//      DSData(VkDescriptorSetLayout a_layout) : layout(a_layout) { sets.reserve(4); }
-//      VkDescriptorSetLayout        layout = {};
-//      std::vector<VkDescriptorSet> sets;
-//    };
+    std::unordered_map<LayoutKey, VkDescriptorSetLayout, LayoutHash> m_layoutDict;
+    std::unordered_map<SetKey, VkDescriptorSet, SetHash> m_setDict;
 
-//    std::unordered_map<PBKey, DSData> m_descriptors;
+    //    struct DSData
+    //    {
+    //      DSData() { sets.reserve(4); }
+    //      DSData(VkDescriptorSetLayout a_layout) : layout(a_layout) { sets.reserve(4); }
+    //      VkDescriptorSetLayout        layout = {};
+    //      std::vector<VkDescriptorSet> sets;
+    //    };
 
+    //    std::unordered_map<PBKey, DSData> m_descriptors;
   };
-}
+}// namespace vk_utils
 
-#endif //VK_SORT_VK_UTILS_H
+#endif// VK_SORT_VK_UTILS_H
